3d2f1fa90ac73fbf8008a28eae2cf5c0f076f869
Koushik <koushik.k@fireflink.com>
2023-04-26 09:52:26 +0530

    abcd dinga dingi

---
+++ src/main/java/com/example/dinga/DingaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dinga/DingaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dinga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
